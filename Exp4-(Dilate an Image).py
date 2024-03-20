84d6240a3366259c10b6df5083878dbe3af81a97
Murali Krishna <124858368+dupakuntlamurali@users.noreply.github.com>
2024-03-20 09:24:25 +0530

    Create Exp4-(Dilate an Image).py

---
+++ Exp4-(Dilate an Image).py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+image = cv2.imread('A1.jpg', cv2.IMREAD_GRAYSCALE)
+kernel = np.ones((3, 3), np.uint8)
+dilated_image = cv2.dilate(image, kernel, iterations=1)
+cv2.imshow('Original Image', image)
+cv2.imshow('Dilated Image', dilated_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
